1290e1fb6c9457e4857e1ebfd5f934f80c92b9ed
ThiagoPedrozo01 <thiago.pedrozo01@fatec.sp.gov.br>
2020-10-12 23:18:54 -0300

    ultima versao

---
+++ src/testeambienteengen/TesteClasse3.java
@@ -0,0 +1,15 @@
+
+package testeambienteengen;
+
+
+public class TesteClasse3 {
+        
+      public printaMensagem(){
+        
+        System.out.println(" Olá mundo");
+  
+       }
+
+    }
+    
+
