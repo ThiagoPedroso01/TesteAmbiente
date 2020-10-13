a342659127f0001b9397f3d3dd10105cf6344d0e
ThiagoPedrozo01 <thiago.pedrozo01@fatec.sp.gov.br>
2020-10-12 22:23:17 -0300

    segunda versao

---
+++ src/testeambienteengen/TesteClasse2.java
@@ -0,0 +1,14 @@
+
+package testeambienteengen;
+
+
+public class TesteClasse2 {
+        
+      public printaMensagem(){
+        System.out.println("Hello world 2 -->Danillo");
+  
+       }
+
+    }
+    
+
